928310096cc80b487db0d91aa376d8cdd5f8fd70
tiltyash <meinslovescs@gmail.com>
2025-03-31 23:29:57 +0300

    Как же тяжко идет

---
+++ src/test/java/OnlinePaymentTest.java
@@ -0,0 +1,75 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OnlinePaymentTest {
+    private static WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        driver.get("http://mts.by");
+        }
+
+    @Test
+    void testBlockTitle() {
+        WebElement title = driver.findElement(By.xpath("//h2[contains(., 'Онлайн пополнение')]"));
+        assertEquals("Онлайн пополнение\nбез комиссии", title.getText());
+        }
+
+    @Test
+    void testPaymentLogos() {
+        var logos = driver.findElements(By.xpath("//div[contains(@class, 'pay__partners')]//img"));
+        logos.forEach(logo -> assertTrue(logo.isDisplayed()));
+    }
+
+    @Test
+    void testDetailsLink() {
+        String initialUrl = driver.getCurrentUrl();
+        driver.findElement(By.xpath("//a[contains(., 'Подробнее о сервисе')]")).click();
+        assertNotEquals(initialUrl, driver.getCurrentUrl());
+    }
+
+    @Test
+    void testPaymentForm() {
+        driver.findElement(By.xpath("//p[contains(., 'Услуги связи')]"));
+        driver.findElement(By.xpath("//input[@placeholder='Номер телефона']")).sendKeys("297777777");
+        driver.findElement(By.xpath("//input[@placeholder='Сумма']")).sendKeys("0.10");
+        driver.findElement(By.xpath("//button[contains(., 'Продолжить')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement iframe = wait.until(ExpectedConditions.presenceOfElementLocated(By.
+                cssSelector("iframe.bepaid-iframe")));
+        driver.switchTo().frame(iframe);
+
+        WebElement paymentContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.
+                xpath("//app-payment-container")));
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
